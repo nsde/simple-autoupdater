2e7ad4ea8b4878759737141b9fd9e049b840f5c8
nsde <neostyxde@gmail.com>
2021-02-16 15:23:14 +0100

    commit

---
+++ src/autoupdate.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+debug_mode = False
+
+def get(url):
+    response = requests.get(url)
+    return response.text
+
+def apply(path, content):
+    cwd = os.getcwd()
+    open(cwd + "\\" + path, "w").write(content)
+
+def update(from_url, to_path):
+    content = get(from_url)
+    apply(path=to_path, content=content)
+
+if __name__ == "__main__": # Demo
+    update(from_url="", to_path="demo\")
